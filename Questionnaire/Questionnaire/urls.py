3649904a73a4734b4b78dcf36bb9716c8cbb6c57
lizonghang <870644199@qq.com>
2015-12-23 00:01:51 +0800

    12-23 00:01

--- Questionnaire/Questionnaire/urls.py
+++ Questionnaire/Questionnaire/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from question_naire.views import *
 
 urlpatterns = [
-    url(r'/$', home_page),
     url(r'login/$', login),
     url(r'regist/$', register),
     url(r'select/$', choose_template),
+    url(r'', home_page),
 ]

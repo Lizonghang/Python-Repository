e0af19071241fb121d71eabfab2b68f23b92e206
lizonghang <870644199@qq.com>
2015-12-22 23:52:11 +0800

    12-22 23:52

--- Questionnaire/Questionnaire/urls.py
+++ Questionnaire/Questionnaire/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from question_naire.views import *
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', include('question_naire.urls')),
+    url(r'/$', home_page),
+    url(r'login/$', login),
+    url(r'regist/$', register),
+    url(r'select/$', choose_template),
 ]
--- Questionnaire/question_naire/urls.py
+++ Questionnaire/question_naire/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import include, url
-from views import *
 
 urlpatterns = [
-    url(r'$', home_page),
-    url(r'login/$', login),
-    url(r'regist/$', register),
-    url(r'select/$', choose_template),
+    
 ]

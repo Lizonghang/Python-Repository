c902406ebdd1b8fbf025791220c12931c56040ed
lizonghang <870644199@qq.com>
2016-01-19 19:29:36 +0800

    modified

--- Questionnaire/Questionnaire/urls.py
+++ Questionnaire/Questionnaire/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from question_naire.views import *
 
 urlpatterns = [
-    url(r'login/$', login),
     url(r'regist/$', register),
     url(r'select/$', choose_template),
     url(r'select/agreement/$', agreement),
--- Questionnaire/question_naire/views.py
+++ Questionnaire/question_naire/views.py
@@ -5,10 +5,6 @@ def home_page(request):
     return render_to_response("questionnaire.html")
 
 
-def login(request):
-    return render_to_response("login.html")
-
-
 def register(request):
     return render_to_response("register.html")
 

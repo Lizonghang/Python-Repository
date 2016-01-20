532c90b186325b3cf8266d940c96b4a85c3221ab
lizonghang <870644199@qq.com>
2016-01-20 10:30:16 +0800

    add success

--- Questionnaire/Questionnaire/urls.py
+++ Questionnaire/Questionnaire/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from question_naire.views import *
 
 urlpatterns = [
-    url(r'regist/$', register),
-    url(r'select/$', choose_template),
-    url(r'select/agreement/$', agreement),
+    url(r'^regist/$', register),
+    url(r'^select/$', choose_template),
+    url(r'^select/agreement/$', agreement),
+    url(r'^select/template_1/$', template_1),
+    url(r'^select/success$', submit_success),
     url(r'', home_page),
 ]
--- Questionnaire/question_naire/views.py
+++ Questionnaire/question_naire/views.py
@@ -15,3 +15,11 @@ def choose_template(request):
 
 def agreement(request):
     return render_to_response("agreement.html")
+
+
+def template_1(request):
+    return render_to_response("template.html")
+
+
+def submit_success(request):
+    return render_to_response("Success.html")

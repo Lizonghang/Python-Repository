9de430db6b981cf6391c0683732c705f73fa63be
lizonghang <870644199@qq.com>
2016-01-28 20:51:05 +0800

    m

--- Questionnaire/question_naire/views.py
+++ Questionnaire/question_naire/views.py
@@ -32,9 +32,10 @@ def edit_template_1(request):
 
 
 def view(request):
-    if request.method == 'POST':
-        # pageForm = simplejson.loads(request.POST.get('pageForm'))
-        pageForm = request.POST.get('pageForm')
-        return render_to_response("Success.html", {'pageForm': pageForm})
-    else:
-        return render_to_response("Success.html")
+    return render_to_response('Success.html', {'pageForm': 'position_1'})
+    # if request.method == 'POST':
+    #     # pageForm = simplejson.loads(request.POST.get('pageForm'))
+    #     pageForm = request.POST.get('pageForm')
+    #     return render_to_response("Success.html", {'pageForm': pageForm})
+    # else:
+    #     return render_to_response("Success.html")

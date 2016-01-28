b7cd21ef10a96771003ab9836648f9f38d101af8
lizonghang <870644199@qq.com>
2016-01-28 16:25:51 +0800

    modified

--- Questionnaire/question_naire/views.py
+++ Questionnaire/question_naire/views.py
@@ -33,7 +33,8 @@ def edit_template_1(request):
 
 def view(request):
     if request.method == 'POST':
-        pageForm = simplejson.loads(request.POST.get('pageForm'))
-        return render_to_response("Success.html", {'pageForm': pageForm}, context_instance=RequestContext(request))
+        return render_to_response('agreement.html')
+        # pageForm = simplejson.loads(request.POST.get('pageForm'))
+        # return render_to_response("Success.html", {'pageForm': pageForm}, context_instance=RequestContext(request))
     else:
         return render_to_response("Success.html")

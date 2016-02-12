fda1e18da515c821d7072f0d238b32f461058392
lizonghang <870644199@qq.com>
2016-02-12 17:40:31 +0800

    m

--- Questionnaire/question_naire/views.py
+++ Questionnaire/question_naire/views.py
@@ -74,7 +74,7 @@ def analysis(request):
         for j in range(0, len(arr[i])):
             if arr[i][j] == '1' or arr[i][j] == '0':
                 arr[i][j] = int(arr[i][j])
-    if UserDefine.objects.get(username="Hang").statics_set.all():
+    if UserDefine.objects.get(username="Hang").statics_set.get(key='arr[0][0]'):
         for m in range(0, len(ans)):
             if len(arr[m]) == 1:
                 foreign_key = 'arr[' + str(m) + '][0]'

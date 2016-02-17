6b7f14f7efb8ab82bb082d534ab869e5753c058e
lizonghang <870644199@qq.com>
2016-02-17 23:47:59 +0800

    m

---
+++ Questionnaire/question_naire/models.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from django.db import models
+
+
+class UserDefine(models.Model):
+    username = models.CharField(max_length=20)
+    pageForm = models.TextField(blank=True, null=True)
+    user_account = models.CharField(max_length=20)
+    passkey = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.username
+
+
+class Statics(models.Model):
+    key = models.CharField(max_length=10)
+    intValue = models.IntegerField(default=0)
+    strValue = models.TextField(default="")
+    user = models.ForeignKey(UserDefine)
+    QContent = models.TextField(default="")
+    QType = models.TextField(default="")
+    dim = models.CharField(default="", max_length=10)
+
+    def __unicode__(self):
+        return self.key
+
+
+class AnsCount(models.Model):
+    question = models.ForeignKey(Statics)
+    key = models.CharField(max_length=20, default="")
+    multi_count = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return u'多选题' + str(self.multi_count) + u'道'

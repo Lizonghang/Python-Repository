e751b0bb668f4355ca1dcb129bfc5d260a87c66d
lizonghang <870644199@qq.com>
2016-02-25 17:43:13 +0800

    VERSION 2.5

--- Questionnaire/question_naire/admin.py
+++ Questionnaire/question_naire/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from question_naire.models import *
 
 
+class UserDefineAdmin(admin.ModelAdmin):
+    list_display = ('username', 'QCount', 'headImgSrc')
+    search_fields = 'username'
+
+
 admin.site.register(UserDefine)
 admin.site.register(Question)
+admin.site.register(UserDefine, UserDefineAdmin)
